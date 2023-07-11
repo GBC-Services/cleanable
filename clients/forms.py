9d6c1091737efe33e1095070c4b3c2a6109da760
AleksTerentyev <Aleks.Terentyev@gmail.com>
2023-07-11 21:22:12 +0300

    Fix for a city on a zip code object

--- clients/forms.py
+++ clients/forms.py
@@ -70,5 +70,5 @@ class PlaceForm(forms.ModelForm):
     def clean_zip_code(self):
         city = self.cleaned_data.get("city")
         zip_code = self.cleaned_data.get("zip_code")
-        zip_code, _ = ZipCode.objects.get_or_create(city=city, value=zip_code.upper())
+        zip_code, _ = ZipCode.objects.get_or_create(value=zip_code.upper())
         return zip_code
